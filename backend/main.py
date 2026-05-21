8ad7bda3c3420cc83da6ef7e64748cd34e512164
Avi Aaron Batchu <aviaaron2003@gmail.com>
2026-05-21 18:35:19 -0500

    bug fixes

--- backend/main.py
+++ backend/main.py
@@ -19,7 +19,7 @@ SECRET_KEY = os.getenv("SECRET_KEY", "development_secret_key")
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 60
 
-pwd_context = CryptContext(schemes=["bcrypt_sha256"], deprecated="auto")
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 
